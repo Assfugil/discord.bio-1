cd5b632245c8df414b0272d96fc1025288e92f11
UndecidedFactor <53883918+UndecidedSource@users.noreply.github.com>
2020-02-21 14:00:22 -0500

    Update check.js

--- src/commands/check.js
+++ src/commands/check.js
@@ -8,9 +8,10 @@ module.exports.run = async function (client, message, args) {
 	let target = args[0];
 	if (!target) return message.channel.send("You must provide a slug to check.")
 	let tBio = await bio.fetchUserDetails(target);
-
 	if (!tBio.success === 1) return message.channel.send(new RichEmbed().setDescription("<:available:680486631484162056> The provided slug is **available**."));
-	return message.channel.send(new RichEmbed().setDescription("<:taken:680486604711788577> The provided slug is **taken**."));
+
+	let user = client.fetchUser(tBio.settings.user_id)
+	return message.channel.send(new RichEmbed().setDescription(`<:taken:680486604711788577> The provided slug is **taken** by **${user.tag}**.`));
 };
 
 module.exports.data = {
